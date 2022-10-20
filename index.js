4ad4ce2c6003f858323747d1f87d2fe7733f1921
dilusha07 <dilushasandaruwani07@gmail.com>
2022-10-20 13:12:55 +0530

    Implement api

--- index.js
+++ index.js
@@ -1,8 +1,12 @@
 const express = require("express");
 const mongoose = require("mongoose");
+const { json } = require("express");
 
 const app = express();
 
+//const Task = require("/models/tasks");
+
+//Connect mongoDB database
 const databaseURL =
   "mongodb+srv://dilusha:dilusha@cluster0.ewvoyv4.mongodb.net/?retryWrites=true&w=majority";
 
@@ -21,3 +25,71 @@ mongoose
   .catch((err) => {
     console.log(err);
   });
+
+let tasks = [
+  {
+    id: 1,
+    title: "Task1",
+    status: "pending",
+    dateAdded: "2022/10/12",
+    dateUpdated: "2022/10/20",
+  },
+
+  {
+    id: 2,
+    title: "Task2",
+    status: "pending",
+    dateAdded: "2022/10/12",
+    dateUpdated: "2022/10/20",
+  },
+  {
+    id: 3,
+    title: "Task3",
+    status: "pending",
+    dateAdded: "2022/10/12",
+    dateUpdated: "2022/10/20",
+  },
+  {
+    id: 4,
+    title: "Task4",
+    status: "pending",
+    dateAdded: "2022/10/12",
+    dateUpdated: "2022/10/20",
+  },
+];
+
+//Get All tasks
+app.get(`/`, (req, res) => {
+  return res.json(tasks);
+});
+
+//Get task by id
+app.get("/task/:id", async (req, res) => {
+  const id = req.params.id;
+  //const task = tasks.find((task) => task.id === id);
+  //console.log(task);
+  return res.json(id);
+});
+
+//Edit task
+app.put("/task/:id", async (req, res) => {
+  const id = req.params.id;
+  const { title, status } = req.body;
+  const taskIndex = tasks.findIndex((task) => task.id === id);
+  tasks[taskIndex] = {
+    ...tasks[taskIndex],
+    title,
+    status,
+  };
+
+  res.send(tasks[taskIndex]);
+});
+
+//Delete task
+app.delete("/task/:id", async (req, res) => {
+  const id = req.params.id;
+  tasks = tasks.filter((task) => task.id !== id);
+  res.send(id);
+  console.log(tasks);
+  //return res.send(id);
+});
